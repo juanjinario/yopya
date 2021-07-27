e0f307f87a86ac053239d14d29c67820bfeb770c
juanjinario <juanjco2@gmail.com>
2021-07-27 18:29:04 +0200

    Add login structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
-  { path: '', redirectTo: 'pages', pathMatch: 'full' },
-  { path: '**', redirectTo: 'pages' },
+  { 
+    path: 'auth', 
+    loadChildren: () => import('./core/auth/auth.module').then(m => m.AuthModule) 
+  },
+  { 
+    path: 'pages', 
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) 
+  },
+  { 
+    path: '', 
+    redirectTo: 'pages', 
+    pathMatch: 'full' },
+  { 
+    path: '**', 
+    redirectTo: 'pages' },
 ];
 
 @NgModule({
---
+++ src/app/core/auth/auth-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth.component';
+
+const routes: Routes = [
+  { 
+    path: '',
+    component: AuthComponent ,
+    children: [
+      { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, 
+      { path: 'reset-password', loadChildren: () => import('./pages/reset-password/reset-password.module').then(m => m.ResetPasswordModule) }
+    ]
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
